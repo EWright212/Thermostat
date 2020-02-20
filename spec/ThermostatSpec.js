cc704415282d7efc78c9420eff071bf1eca17f5d
Student <emilywright212@gmail.com>
2020-02-20 14:38:02 +0000

    Added index file

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,3 +1,5 @@
+'use strict';
+
 describe("Thermostat", function () {
   beforeEach(function() {
     thermostat = new Thermostat();
@@ -26,9 +28,43 @@ describe("Thermostat", function () {
     thermostat.powerSaverSwitch()
     expect(thermostat.powerSaver).toBe(false);
   });
-  it("has a power saving mode, can turn off", function () {
+  it("has a power saving mode, can turn on", function () {
     thermostat.powerSaverSwitch()
     thermostat.powerSaverSwitch()
     expect(thermostat.powerSaver).toBe(true);
+  });
+  it("has a max temp of 32 degrees if power saver off", function () {
+    thermostat.powerSaverSwitch()
+    expect(thermostat.maxTemp).toEqual(32);
+  });
+  it("has a max temp of 25 degrees if power saver on", function () {
+    expect(thermostat.maxTemp).toEqual(25);
+  });
+  it("can't increase beyond max temp, powerSaver on", function () {
+    thermostat.temperature = 25
+    thermostat.increase()
+    expect(thermostat.maxTemp).toEqual(25);
+  });
+  it("can't increase beyond max temp, powerSaver off", function () {
+    thermostat.powerSaverSwitch()
+    thermostat.temperature = 32
+    thermostat.increase()
+    expect(thermostat.temperature).toEqual(32);
+  });
+  it("can reset temp to 20", function () {
+    thermostat.temperature = 25
+    thermostat.resetSwitch()
+    expect(thermostat.temperature).toEqual(20);
+  });
+  it("can recognise low usage <18 ", function () {
+    thermostat.temperature = 17
+    expect(thermostat.energyUsageChecker()).toEqual("low-usage");
+  });
+  it("can recognise medium usage", function () {
+    expect(thermostat.energyUsageChecker()).toEqual("medium-usage");
+  });
+  it("can recognise high usage >= 25", function () {
+    thermostat.temperature = 25
+    expect(thermostat.energyUsageChecker()).toEqual("high-usage");
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,22 +1,51 @@
 Thermostat = function () {
-  this.temperature = 20;
-  this.mintemp = 10
+  this.standardTemp = 20
+  this.temperature = this.standardTemp;
+  this.minTemp = 10
   this.powerSaver = true
+  this.maxTemp = 25
+  this.energyUsageLow = 18
+  this.energyUsageHigh = 25
 };
 
 var thermostat = new Thermostat();
 
 Thermostat.prototype.increase  = function() {
-  return(this.temperature += 1);
-}
+  if (this.temperature === this.maxTemp) {
+    return;
+  }
+  this.temperature += 1;
+  }
+
 
 Thermostat.prototype.decrease  = function() {
-  if (this.temperature === this.mintemp) {
-  } else {
-    return(this.temperature -= 1);
-  }
+  if (this.temperature === this.minTemp) {
+    return;
+  } 
+  this.temperature -= 1;
 }
 
 Thermostat.prototype.powerSaverSwitch  = function() {
-  this.powerSaver = (this.powerSaver ? false : true);
+  if (this.powerSaver == true) {
+    this.powerSaver = false;
+    this.maxTemp = 32;
+  } else {
+    this.powerSaver = true;
+    this.maxTemp = 25;
+  }
 }
+
+Thermostat.prototype.resetSwitch  = function() {
+  this.temperature = this.standardTemp;
+}
+
+Thermostat.prototype.energyUsageChecker = function() {
+  if (this.temperature < this.energyUsageLow) {
+    return "low-usage";
+  } else if (this.temperature >= this.energyUsageHigh) {
+    return "high-usage";
+  } else {
+    return "medium-usage";
+  }
+}
+
